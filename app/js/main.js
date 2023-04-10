f7d2c4c5fe822a139e7d1142708c7896d69098d3
Mirzoavaz <73744057+Number1ava@users.noreply.github.com>
2023-04-10 09:48:18 +0500

    add new videos on resources.html

--- app/js/main.js
+++ app/js/main.js
@@ -269,4 +269,45 @@ window.addEventListener('DOMContentLoaded', () => {
     }
   }
   burgerMenu(".burger__menu");
+
+  // videos youtube
+  const videoInit = (selector) => {
+    const videos = document.querySelectorAll(selector)
+
+    if (videos.length > 0) {
+      videos.forEach(video => {
+        videoGenerate(video)
+      })
+    }
+
+  }
+
+  const videoGenerate = (video) => {
+    const btn = video.querySelector(".video__block-button")
+    const videoId = btn.dataset.videoId
+    const container = video.querySelector('.video__block-inner')
+
+    btn.addEventListener('click', () => {
+      const iframe = iframeGenerate(videoId)
+
+      container.innerHTML = '';
+      container.appendChild(iframe)
+    })
+  }
+
+  const iframeGenerate = (videoId) => {
+    const iframe = document.createElement('iframe')
+
+    const src = `https://www.youtube.com/embed/${videoId}?rel=0&showinfo=0&autoplay=1`
+
+    iframe.setAttribute('src', src)
+    iframe.setAttribute('frameborder', "0")
+    iframe.setAttribute('allow', "autoplay")
+    iframe.setAttribute('allowfullscreen', "")
+    iframe.classList.add('video__block-content')
+
+    return iframe
+  }
+  videoInit('.video__block')
+
 });
